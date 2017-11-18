fef1b659083dc14e1a401d623806788fb3f08995
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-11-18 03:06:25 +0300

    Add flag "testMode" - manual input of hour.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -13,6 +14,7 @@ import (
 // Returns greeting text according to get current hour
 // It need a parameter int hour - 0...24
 func getGreetings(hour int) (string, error) {
+
 	var message string
 	switch {
 	case (hour >= 6) && (hour < 12):
@@ -31,23 +33,71 @@ func getGreetings(hour int) (string, error) {
 	return message, nil
 }
 
-/*
-	TODO
+// User input of hour
+// returns error if not int entered and looped while not succeed
+func setHour() int {
 
+	var hour int
+	var isEntered bool
+	for !isEntered {
+		fmt.Printf("Please enter the hour: ")
+		_, err := fmt.Scan(&hour)
 
-*/
+		if err != nil {
+			color.Red(err.Error()) // print error message "err" in color red if not integer was inputted
+
+		} else {
+			isEntered = true
+		}
+
+	}
+
+	fmt.Printf("\n\n\n")
+	return hour
+}
+
+// Get data and time values
+func getCurrentTime() (int, int, int, string, string) {
+
+	hour := time.Now().Hour()
+	minutes := time.Now().Minute()
+
+	day := time.Now().Day()
+	weekday := time.Now().Weekday().String()
+	month := time.Now().UTC().Format("January")
+
+	return hour, minutes, day, weekday, month
+}
+
+// Main function that runs the program
 func main() {
-	hourOfDay := time.Now().Hour()
-	minutesOfDay := time.Now().Minute()
-	//timeNow := strconv.Itoa( hourOfDay )+":"+strconv.Itoa( minutesOfDay )
+	var isTestMode bool
+	flag.BoolVar(&isTestMode, "testMode", false, "Turns on test mode of application. Ability to input manually hour")
+	flag.Parse()
 
-	todayDay := time.Now().Day()
-	weekdayOfDay := time.Now().Weekday().String()
-	monthNow := time.Now().UTC().Format("January")
-	//dateNowMessage := "Today is: " + weekdayOfDay[ 0:3 ] + ", " + strconv.Itoa( todayDay ) + " of " + monthNow
+	var hourOfDay, minutesOfDay, todayDay int
+	var weekdayOfDay, monthNow string
 
-	fmt.Printf("It's %d:%d now \n", hourOfDay, minutesOfDay)
-	fmt.Printf("Today is: %s, %d of %s \n", weekdayOfDay, todayDay, monthNow)
+	todayDay = -1     // set as default incorrect value
+	minutesOfDay = -1 // set as default incorrect value
+	hourOfDay = -1    // set as default incorrect value
+
+	if isTestMode {
+		fmt.Printf("In a test mode now!\n\n")
+		_, minutesOfDay, todayDay, weekdayOfDay, monthNow = getCurrentTime()
+		hourOfDay = setHour()
+
+		fmt.Printf("It's %d:%d now \n", hourOfDay, minutesOfDay)
+		fmt.Printf("Today is: %s, %d of %s \n\n", weekdayOfDay, todayDay, monthNow)
+
+	} else {
+		fmt.Printf("In ususal mode...\n\n")
+
+		hourOfDay, minutesOfDay, todayDay, weekdayOfDay, monthNow = getCurrentTime()
+
+		fmt.Printf("It's %d:%d now \n", hourOfDay, minutesOfDay)
+		fmt.Printf("Today is: %s, %d of %s \n\n", weekdayOfDay, todayDay, monthNow)
+	}
 
 	greeting, err := getGreetings(hourOfDay)
 
