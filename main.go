7270dfdfe7bbb7e225f38454084fc3d6e8400646
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-11-20 02:13:38 +0300

    Add errors types detection

--- main.go
+++ main.go
@@ -14,6 +14,41 @@ import (
 	"github.com/fatih/color"
 )
 
+var (
+	// ErrInput indicates that a value is out of range for the target type.
+	ErrInput = errors.New("should be integer in range [0;24]")
+
+	// ErrGreeting indicates that a value does not have the right syntax for the target type.
+	ErrGreeting = errors.New("hour out of range [0;24]")
+)
+
+// GretingsError records for what input error ocurs and with what value
+type GreetingsError struct {
+	Err       error  // the reason why failed (ErrInput, ErrGreeting)
+	HourInput string // the user input
+}
+
+// function constructing error message from  GreetingsError struct
+func (e *GreetingsError) Error() string {
+	return "[ERROR] " + " '" + e.HourInput + "' " + e.Err.Error()
+}
+
+// function sets structure GreetingsError
+func inputError(str string) *GreetingsError {
+	return &GreetingsError{
+		ErrInput,
+		str,
+	}
+}
+
+// function sets structure GreetingsError
+func greetingError(str string) *GreetingsError {
+	return &GreetingsError{
+		ErrGreeting,
+		str,
+	}
+}
+
 // error catcher - returns true value
 func checkError(err error) bool {
 	var errorExist bool
@@ -33,6 +68,8 @@ func checkError(err error) bool {
 func getGreetings(hour int) (string, error) {
 
 	var message string
+	var err error
+
 	switch {
 	case (hour >= 6) && (hour < 12):
 		message = "Good morning!"
@@ -43,56 +80,46 @@ func getGreetings(hour int) (string, error) {
 	case (((hour >= 22) && (hour <= 24)) || ((hour >= 0) && (hour < 6))):
 		message = "Good night!"
 	default:
-		err := errors.New("[ERROR] Incorrect value for hour: " + strconv.Itoa(hour))
+		//err := errors.New("[ERROR] Incorrect value for hour: " + strconv.Itoa(hour))
+		err = greetingError(strconv.Itoa(hour))
 		return message, err
 	}
 
-	return message, nil
+	return message, err
 }
 
 // User input of hour
 // returns error if not int entered and looped while not succeed
-func setHour() int {
+func setHour() (bool, int, error) {
 
-	var hour int
-	var input string
-	var errReader, errAtoi error
+	var (
+		hour      int    // hour that is get from converting user input to integer
+		input     string // user input
+		errReader error  // error for reader.ReadString - input
+		errAtoi   error  // error for strconv.Atoi - converting of user string input to integer
+		err       error  // error message that will be returned from function setHour
 
-	var isEntered bool
-	for !isEntered {
+		isEntered bool // indicate if input was successful
+	)
 
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Printf("Enter text: ")
-		input, errReader = reader.ReadString('\n')
-		input = strings.Replace(input, "\n", "", -1)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter text: ")
+	input, errReader = reader.ReadString('\n')
+	input = strings.Replace(input, "\n", "", -1)
 
-		if !(checkError(errReader)) {
-			hour, errAtoi = strconv.Atoi(input) // convert user string input to int
-			if !(checkError(errAtoi)) {
-				isEntered = true // check if conversation was correct - exit loop of input
-
-			}
-
-		}
-
-		/* if errReader != nil {
-			color.Red(errReader.Error()) // print error message "err" in color red if not integer was inputted
+	if !(checkError(errReader)) {
+		hour, errAtoi = strconv.Atoi(input) // convert user string input to int
+		if !(checkError(errAtoi)) {
+			isEntered = true // check if conversation was correct - exit loop of input
 
 		} else {
-			hour, errAtoi = strconv.Atoi(input)
-			if errAtoi != nil {
-				color.Red(errAtoi.Error() + " - should be integer number\n") // print error message "err" in color red if not integer was inputted
-				color.Yellow("Try again...\n\n")
-
-			} else {
-				isEntered = true
-			}
-
-		} */
+			err = inputError(input)
+		}
 
 	}
+
 	fmt.Printf("\n\n\n")
-	return hour
+	return isEntered, hour, err
 }
 
 // Get data and time values
@@ -116,6 +143,8 @@ func main() {
 
 	var hourOfDay, minutesOfDay, todayDay int
 	var weekdayOfDay, monthNow string
+	var isEntered bool
+	var err error
 
 	todayDay = -1     // set as default incorrect value
 	minutesOfDay = -1 // set as default incorrect value
@@ -124,7 +153,16 @@ func main() {
 	if isTestMode {
 		fmt.Printf("In a test mode now!\n\n")
 		_, minutesOfDay, todayDay, weekdayOfDay, monthNow = getCurrentTime()
-		hourOfDay = setHour()
+
+		for {
+			isEntered, hourOfDay, err = setHour()
+			if isEntered {
+				break
+			} else {
+				checkError(err)
+				color.Yellow("Try again...\n\n")
+			}
+		}
 
 		fmt.Printf("It's %d:%d now \n", hourOfDay, minutesOfDay)
 		fmt.Printf("Today is: %s, %d of %s \n\n", weekdayOfDay, todayDay, monthNow)
@@ -145,12 +183,5 @@ func main() {
 	} else {
 		fmt.Println(greeting)
 	}
-
-	/* if err != nil {
-		color.Red(err.Error()) // print error message "err" in color red
-		log.Printf(err.Error())
-		os.Exit(1)
-
-	} */
 
 }
