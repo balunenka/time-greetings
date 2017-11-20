f3d4c7e397ff121b09ffa5fb852dca82bc90f7a3
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-11-20 12:30:20 +0300

    Fix run-build.sh: move bins to bin dir, disable zip

--- run-build.sh
+++ run-build.sh
@@ -2,19 +2,21 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKAGE_NAME=$(basename "$SCRIPT_DIR")
+mkdir -p ${SCRIPT_DIR}/bin
+PACKAGE_BIN_DIR=${SCRIPT_DIR}/bin
 
-rm ${PACKAGE_NAME}-*
+rm ${PACKAGE_BIN_DIR}/${PACKAGE_NAME}-*
 
 GOOS=darwin
 go build
-mv ${PACKAGE_NAME} ${PACKAGE_NAME}-${GOOS}
+mv ${PACKAGE_NAME} ${PACKAGE_BIN_DIR}/${PACKAGE_NAME}-${GOOS}
 
 GOOS=linux
 go build
-mv ${PACKAGE_NAME} ${PACKAGE_NAME}-${GOOS}
+mv ${PACKAGE_NAME} ${PACKAGE_BIN_DIR}/${PACKAGE_NAME}-${GOOS}
 
 GOOS=windows
 go build
-mv ${PACKAGE_NAME}.exe ${PACKAGE_NAME}-${GOOS}.exe
+mv ${PACKAGE_NAME}.exe ${PACKAGE_BIN_DIR}/${PACKAGE_NAME}-${GOOS}.exe
 
-zip ${PACKAGE_NAME}.zip ${PACKAGE_NAME}-*
+# zip ${PACKAGE_NAME}.zip ${PACKAGE_NAME}-*
